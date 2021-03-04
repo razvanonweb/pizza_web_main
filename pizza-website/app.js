52e59e85520bfa9a0677e5b36b02feffd562c2b0
razvanxx11 <dumitru.razvan230796@gmail.com>
2021-03-04 17:26:48 +0200

    fix source path

--- pizza-website/app.js
+++ pizza-website/app.js
@@ -1,103 +1,89 @@
-// redesign nav 
-const lineForMenu = document.querySelector(".height"); 
-const menu = document.querySelector(".menu");
+// redesign nav
+const lineForMenu = document.querySelector('.height');
+const menu = document.querySelector('.menu');
 
 // animation on reload
-const loading = document.querySelector("#loader"); 
+const loading = document.querySelector('#loader');
 
-// hamburger 
+// hamburger
 
-const hamburger = document.querySelector(".hamburger");
+const hamburger = document.querySelector('.hamburger');
 
-// close menu on click/touch on small media screen 
+// close menu on click/touch on small media screen
 
-const bodyNav = document.querySelector("body nav");
+const bodyNav = document.querySelector('body nav');
 
+// redesign nav
 
-// redesign nav 
-
-const stickyNav = () =>{
-    window.addEventListener("scroll" , ()=> {
-        const top = window.pageYOffset;
-        if(top > 100) {
-            lineForMenu.style.height = "45px";
-            lineForMenu.style.boxShadow = "0px 0px 5px";
-            menu.style.top = "45px";
-            menu.style.transition = "top 0.3s ease-in-out"
-            hamburger.style.transform = "translate(-50%, 7%)"
-            
-        } else {
-            lineForMenu.style.height = "54px";
-            lineForMenu.style.boxShadow = "unset";
-            menu.style.top = "54px";
-            menu.style.transition = "top 0.3s ease-in-out"
-            hamburger.style.transform = "translate(-50%, 30%)"
-        }
-    });
+const stickyNav = () => {
+  window.addEventListener('scroll', () => {
+    const top = window.pageYOffset;
+    if (top > 100) {
+      lineForMenu.style.height = '45px';
+      lineForMenu.style.boxShadow = '0px 0px 5px';
+      menu.style.top = '45px';
+      menu.style.transition = 'top 0.3s ease-in-out';
+      hamburger.style.transform = 'translate(-50%, 7%)';
+    } else {
+      lineForMenu.style.height = '54px';
+      lineForMenu.style.boxShadow = 'unset';
+      menu.style.top = '54px';
+      menu.style.transition = 'top 0.3s ease-in-out';
+      hamburger.style.transform = 'translate(-50%, 30%)';
+    }
+  });
 };
 
-stickyNav()
+stickyNav();
 
 // animation on reload
 
 const onReload = () => {
-    window.addEventListener("load", () => {
-        setTimeout(function () {
-            loading.style.display = "none";
-        }, 2200)
-    });
+  window.addEventListener('load', () => {
+    setTimeout(function () {
+      loading.style.display = 'none';
+    }, 2200);
+  });
 };
 
-onReload(); 
+onReload();
 
-// section 1 slider 
+// section 1 slider
 
 const sliderSection1 = () => {
-    $(".center").slick({
-        dots: false,
-        infinite: false,
-        centerMode: false,
-    });
-}
+  $('.center').slick({
+    dots: false,
+    infinite: false,
+    centerMode: false,
+  });
+};
 
 sliderSection1();
 
-
-// hamburger 
+// hamburger
 
 const showMenuOnMObile = () => {
-    hamburger.addEventListener('click', () => {
-
-        hamburger.classList.toggle("changeHamburger");
-        menu.classList.toggle("openMenu");
-        menu.style.transition = "transform 0.3s ease-in-out";
-
-    })
-}
+  hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('changeHamburger');
+    menu.classList.toggle('openMenu');
+    menu.style.transition = 'transform 0.3s ease-in-out';
+  });
+};
 
 showMenuOnMObile();
 
-
-// close menu on click/touch on small media screen 
+// close menu on click/touch on small media screen
 
 const closeMenuOutside = () => {
-
-    document.addEventListener("click", (e) => {
-
-        if(bodyNav.contains(e.target)) {
-            // ok
-
-        } else {
-
-            hamburger.classList.remove("changeHamburger");
-            menu.classList.remove("openMenu");
-            menu.style.transition = "transform 0.3s ease-in-out";
-        }
-
-    })
-
-
-}
+  document.addEventListener('click', (e) => {
+    if (bodyNav.contains(e.target)) {
+      // ok
+    } else {
+      hamburger.classList.remove('changeHamburger');
+      menu.classList.remove('openMenu');
+      menu.style.transition = 'transform 0.3s ease-in-out';
+    }
+  });
+};
 
 closeMenuOutside();
-
